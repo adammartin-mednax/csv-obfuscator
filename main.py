b43ffb60bb6540f26d16bcc9d60b145880f0f098
adammartin <ebondark.od@gmail.com>
2022-05-16 13:11:33 -0400

    fix to a defect on the columns to obfuscate

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from csv_obfuscator import process
 
 def main():
     input_file_name = sys.argv[1]
-    columns_to_obfuscate = map(int, sys.argv[2].split(","))
+    columns_to_obfuscate = list(map(int, sys.argv[2].split(",")))
     delimiter = (sys.argv[3] if len(sys.argv) > 3 else ",")
 
     process(input_file_name, "output.csv", delimiter, columns_to_obfuscate)
