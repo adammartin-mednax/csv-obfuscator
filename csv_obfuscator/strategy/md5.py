e6b4394d7b1124913d96ca01fd6b4b6a143a1dc7
adammartin <ebondark.od@gmail.com>
2022-05-16 13:11:33 -0400

    Remove dead code

--- csv_obfuscator/strategy/md5.py
+++ csv_obfuscator/strategy/md5.py
@@ -1,12 +1,6 @@
 import hashlib
 
 
-def obfuscate(value):
-    m = hashlib.new('md5', usedforsecurity=False)
-    m.update(value.encode('utf-8'))
-    return m.hexdigest()
-
-
 class MD5:
     def __init__(self, arguments):
         self._hash_type = arguments['strategy']
--- tests/strategy/test_md5.py
+++ tests/strategy/test_md5.py
@@ -1,10 +1,5 @@
 import re
-from csv_obfuscator.strategy.md5 import MD5, obfuscate
-
-def test_will_hash_imput():
-    value = 'example value'
-    md5_regex_validation = re.compile(r'^[A-Fa-f0-9]{32}$', flags=re.M)
-    assert len(md5_regex_validation.findall(obfuscate(value))) == 1
+from csv_obfuscator.strategy.md5 import MD5
 
 
 def test_will_hash_input():
