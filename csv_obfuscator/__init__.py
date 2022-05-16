c4d2037e85c3afffa2183e6c4a07db09084c3798
adammartin <ebondark.od@gmail.com>
2022-05-16 13:11:32 -0400

    Clean up of tests for readability

--- csv_obfuscator/__init__.py
+++ csv_obfuscator/__init__.py
@@ -17,9 +17,9 @@ def obfuscate(csv_input_stream, csv_output_stream, colums_to_obfuscate, strategy
         csv_output_stream.writerow(new_row)
 
 
-def orchestrator(csv_input_file, csv_output_file, delimiter, columns_to_obfuscate):
-    with open(csv_output_file, mode='w') as output:
-        with open(csv_input_file) as csv_input:
-            csv_output = csv.writer(output, delimiter=delimiter)
-            csv_reader = csv.reader(csv_input, delimiter=delimiter)
-            obfuscate(csv_reader, csv_output, columns_to_obfuscate, md5_strategy)
+def orchestrator(input_file_name, output_file_name, delimiter, columns_to_obfuscate):
+    with open(output_file_name, mode='w') as output_stream:
+        with open(input_file_name) as input_stream:
+            csv_writer = csv.writer(output_stream, delimiter=delimiter)
+            csv_reader = csv.reader(input_stream, delimiter=delimiter)
+            obfuscate(csv_reader, csv_writer, columns_to_obfuscate, md5_strategy)
--- tests/test_csv_processor.py
+++ tests/test_csv_processor.py
@@ -12,49 +12,38 @@ file_stream = 'blah'
 @patch('csv_obfuscator.obfuscate')
 @patch('csv.writer')
 @patch('csv.reader')
-def test_orchestrator_will_open_file_to_write_to_and_read_from(mock_reader, mock_writer, mock_processor, mock_file):
+def test_orchestrator_will_open_file_to_write_to_and_read_from(mock_reader, mock_writer, mock_obfuscate, mock_file):
     csv_input_file = 'input'
     csv_output_file = 'output'
-    delimiter = ','
-    columns_to_obfuscate = [1, 7, 9]
-    orchestrator(csv_input_file, csv_output_file, delimiter, columns_to_obfuscate)
+    orchestrator(csv_input_file, csv_output_file, ',', [1, 7, 9])
     mock_file.assert_has_calls([call(csv_output_file, mode='w'), call(csv_input_file)], any_order=True)
 
 @patch('builtins.open', new_callable=mock_open, read_data=file_stream)
 @patch('csv_obfuscator.obfuscate')
 @patch('csv.writer')
 @patch('csv.reader')
-def test_orchestrator_will_create_a_csv_writer(mock_reader, mock_writer, mock_processor, mock_file):
-    csv_input_file = 'input'
-    csv_output_file = 'output'
+def test_orchestrator_will_create_a_csv_writer(mock_reader, mock_writer, mock_obfuscate, mock_file):
     delimiter = ','
-    columns_to_obfuscate = [1, 7, 9]
-    orchestrator(csv_input_file, csv_output_file, delimiter, columns_to_obfuscate)
+    orchestrator('input', 'output', delimiter, [1, 7, 9])
     mock_writer.assert_called_once_with(mock_file.return_value, delimiter=delimiter)
 
 @patch('builtins.open', new_callable=mock_open, read_data=file_stream)
 @patch('csv_obfuscator.obfuscate')
 @patch('csv.writer')
 @patch('csv.reader')
-def test_orchestrator_will_create_a_csv_reader(mock_reader, mock_writer, mock_processor, mock_file):
-    csv_input_file = 'input'
-    csv_output_file = 'output'
+def test_orchestrator_will_create_a_csv_reader(mock_reader, mock_writer, mock_obfuscate, mock_file):
     delimiter = ','
-    columns_to_obfuscate = [1, 7, 9]
-    orchestrator(csv_input_file, csv_output_file, delimiter, columns_to_obfuscate)
+    orchestrator('input', 'output', delimiter, [1, 7, 9])
     mock_reader.assert_called_once_with(mock_file.return_value, delimiter=delimiter)
 
 @patch('builtins.open', new_callable=mock_open, read_data=file_stream)
 @patch('csv_obfuscator.obfuscate')
 @patch('csv.writer')
 @patch('csv.reader')
-def test_orchestrator_will_process_the_files(mock_reader, mock_writer, mock_processor, mock_file):
-    csv_input_file = 'input'
-    csv_output_file = 'output'
-    delimiter = ','
+def test_orchestrator_will_pass_the_data_to_the_strategy_to_be_obfuscated(mock_reader, mock_writer, mock_obfuscate, mock_file):
     columns_to_obfuscate = [1, 7, 9]
-    orchestrator(csv_input_file, csv_output_file, delimiter, columns_to_obfuscate)
-    mock_processor.assert_called_once_with(mock_reader.return_value,
+    orchestrator('input', 'output', ',', columns_to_obfuscate)
+    mock_obfuscate.assert_called_once_with(mock_reader.return_value,
                                            mock_writer.return_value,
                                            columns_to_obfuscate,
                                            md5_strategy)
